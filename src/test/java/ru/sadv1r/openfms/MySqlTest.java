74429f20fc874547c06c6e94ce36a179b43d53f3
sadv1r <sadv12@gmail.com>
2015-05-08 02:26:26 +0300

    Создан тест для MySQL.class

---
+++ src/test/java/ru/sadv1r/openfms/MySqlTest.java
@@ -0,0 +1,34 @@
+package ru.sadv1r.openfms;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class MySqlTest {
+    private static final String SERVER_NAME = "localhost";
+    private static final int PORT_NUMBER = 3306;
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "";
+    private static final String DATABASE = "openfms";
+    private static MySql mySql;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        mySql = new MySql(SERVER_NAME, PORT_NUMBER, USER_NAME, PASSWORD, DATABASE);
+    }
+
+    @Test
+    public void testConnection() throws Exception {
+        assertTrue(mySql != null);
+    }
+
+    @Test
+    public void testGetVkIdsToParse() throws Exception {
+        assertEquals(1, (int) mySql.getVkIdsToParse(0).get(0));
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        mySql.close();
+    }
+}
