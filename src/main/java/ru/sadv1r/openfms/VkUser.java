22c0ef51e2add2760d17eaada67651d52276b4ff
sadv1r <sadv12@gmail.com>
2015-05-07 18:40:15 +0300

    Рефакторинг Quick Fix

--- src/main/java/ru/sadv1r/openfms/VkUser.java
+++ src/main/java/ru/sadv1r/openfms/VkUser.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
