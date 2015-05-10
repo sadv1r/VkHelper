5f5526e4f187fb1b38485baa0b82de7d79d91a9a
sadv1r <sadv12@gmail.com>
2015-05-10 09:03:39 +0300

    Создан тест для HibernateUtil
    Не работает второй тест, не пересоздается база. Скорее всего не может
    удалить ключи

---
+++ src/test/java/ru/sadv1r/openfms/HibernateTest.java
@@ -0,0 +1,53 @@
+package ru.sadv1r.openfms;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import java.io.IOException;
+
+/**
+ * Created on 5/10/15.
+ *
+ * @author sadv1r
+ * @version 0.1
+ */
+public class HibernateTest {
+    @Before
+    public void setUp() throws Exception {
+        HibernateUtil.buildEntityManagerFactory("OpenfmsPersistenceUnitTest");
+    }
+
+    @Test
+    public void getEntityManagerFactory() throws Exception {
+        assertTrue(HibernateUtil.getEntityManagerFactory().isOpen());
+    }
+
+    /*@Test
+    public void updateDbEntity() throws Exception {
+        VkUser vkUser = new VkUser();
+        vkUser.setPhotoId("0_123");
+        EntityManager entityManager = HibernateUtil.getEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+        entityManager.persist(vkUser);
+        entityTransaction.commit();
+        entityManager.refresh(vkUser);
+        assertEquals(123, vkUser.getPhotoId());
+
+        vkUser.setPhotoId("0_456");
+        entityManager.merge(vkUser);
+        entityManager.refresh(vkUser);
+        assertEquals(456, vkUser.getVkId());
+        entityManager.close();
+    }*/
+
+    @After
+    public void tearDown() throws Exception {
+        HibernateUtil.closeEntityManagerFactory();
+    }
+}
