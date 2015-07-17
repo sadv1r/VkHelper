1b9c85afa09fa3108383700e42b65885ca5d6511
sadv1r <sadv12@gmail.com>
2015-07-17 13:29:51 +0300

    -f флаг добавлен и произведен некоторый рефакторинг

--- src/main/java/ru/sadv1r/openfms/Main.java
+++ src/main/java/ru/sadv1r/openfms/Main.java
@@ -55,8 +55,11 @@ public class Main {
         actionOptionGroup.addOption(new Option("H", "hidden", false, "поиск скрытых друзей"));
         actionOptionGroup.addOption(new Option("gu", "group-users", false, "получить список пользователей группы"));
         actionOptionGroup.addOption(new Option("A", "all", false, "сохраняем всех пользователей (недоступно)"));
-        actionOptionGroup.addOption(Option.builder("i")
-                .longOpt("info")
+        actionOptionGroup.addOption(new Option("i", "info", false, "получить информацию о пользователе"));
+        options.addOptionGroup(actionOptionGroup);
+
+        /*options.addOption(Option.builder("f")
+                .longOpt("fields")
                 .desc("получить информацию о пользователе\n"
                         + "в качестве аргумента можно передать необходимые поля.\n"
                         + "Например: -i id,firstName,lastName\n"
@@ -66,10 +69,24 @@ public class Main {
                         "education,universities,schools,status," +
                         "last_seen,relation,relatives,counters,screen_name,maiden_name,occupation,activities,interests,music,movies," +
                         "tv,books,games,about,quotes,personal,nickname")
+                .hasArgs()
                 .argName("fields")
-                .optionalArg(true)
+                .valueSeparator(',')
+                .build());*/
+
+        options.addOption(Option.builder("f")
+                .longOpt("fields")
+                .desc("установить кастомные поля\n"
+                        + "Например: -а sex,bdate,city\n"
+                        + "Полный список доступных полей:\n"
+                        + "sex,bdate,city,country,"
+                        + "photo_max_orig,online,online_mobile,has_mobile,contacts,connections,site,"
+                        + "education,universities,schools,status,"
+                        + "last_seen,relation,relatives,counters,screen_name,maiden_name,occupation,activities,interests,music,movies,"
+                        + "tv,books,games,about,quotes,personal,nickname")
+                .hasArg()
+                .argName("fields")
                 .build());
-        options.addOptionGroup(actionOptionGroup);
 
         CommandLineParser commandLineParser = new DefaultParser();
         CommandLine commandLine = commandLineParser.parse(options, args);
@@ -81,6 +98,16 @@ public class Main {
 
         /* РАБОТАЕМ С ВК */
         else if (commandLine.hasOption("vk")) {
+            if (commandLine.hasOption("f")) {
+                //String fields[] = commandLine.getOptionValues("f");
+                String fields = commandLine.getOptionValue("f");
+                VkParser.setFieldsToParse(fields);
+
+                /* Вызываем нужный метод через рефлешен
+                Class clazz = vkUser.getClass();
+                System.out.println(clazz.getDeclaredMethod("get" + args[4]).invoke(vkUser));*/
+            }
+
             if (commandLine.hasOption("H")) {
                 String targetDomain = commandLine.getOptionValue("u");
                 int targetId;
@@ -132,120 +159,44 @@ public class Main {
                 }
                 VkUser vkUser = VkParser.parse(targetId);
                 printUserInfo(vkUser);
-            }
-        }
+            } else if (commandLine.hasOption("all")) {
+                HibernateUtil.buildEntityManagerFactory("OpenfmsPersistenceUnit");
+                int a = 1;
+                int[] ids = new int[500];
 
-        /*if ("-vk".equals(args[0])) {
-            if ("-hidden".equals(args[1]) && args.length == 3) {
-                if (args[2].matches("\\d+")) {
-                    HashSet<Integer> hiddenFriends = new HashSet<>();
-
-                    int targetId = Integer.parseInt(args[2]);
-                    ArrayList<Integer> targetFriends = VkParser.parseFriends(targetId); //друзья
-                    int a = 0, b;
-                    try {
-                        for (int targetFriend : targetFriends) {
-                            ArrayList<Integer> friendsOfTargetFriend = VkParser.parseFriends(targetFriend); //друзья друзей
-                            b = 0;
-                            for (int friendOfTargetFriend : friendsOfTargetFriend) {
-                                ArrayList<Integer> friendsOfFriendsOfTargetFriend = VkParser.parseFriends(friendOfTargetFriend); //друзья друзей друзей
-                                for (int friendOfFriendOfTargetFriend : friendsOfFriendsOfTargetFriend) {
-                                    if (targetId == friendOfFriendOfTargetFriend && !targetFriends.contains(friendOfTargetFriend)) {
-                                        hiddenFriends.add(friendOfTargetFriend);
-                                    }
-                                }
-                                System.out.print(a * 100 / targetFriends.size() + "% выполнено всего. " + b++ * 100 / friendsOfTargetFriend.size() +
-                                        "% выполнено на текущей стадии" + "\r");
-                            }
-                            a++;
-                        }
-                        System.out.println();
-                    } catch (Exception e) {
-                        System.out.println("Работа не была завершена. Все, что удалось найти:");
-                        for (int i : hiddenFriends) {
-                            System.out.println(i);
-                        }
-                        throw e;
+                while (true) {
+                    for (int i = 0; i < 500; i++) {
+                        ids[i] = a + i;
+                        System.out.println(a + i);
                     }
 
-                    for (int i : hiddenFriends) {
-                        System.out.println(i);
+                    ArrayList<VkUser> vkUsers = VkParser.parse(ids);
+                    for (VkUser vkUser : vkUsers) {
+                        try { //FIXME! сделать уже что-то с этим костылем
+                            EntityManager entityManager = HibernateUtil.getEntityManager();
+                            EntityTransaction entityTransaction = entityManager.getTransaction();
+                            entityTransaction.begin();
+
+                            entityManager.merge(vkUser);
+
+                            entityTransaction.commit();
+                            entityManager.close();
+                        } catch (Exception e) {
+
+                        }
                     }
+                    a += 500;
                 }
-            }
-        }*/
-
-        /*Если аргументы есть*/
-        if (args.length > 0) {
-            /*Работаем с Вконтакте*/
-            if (args[0].equals("-vk")) {//{args.length >= 2)
-                if (args.length == 2) {
-                    /*Полный парсинг пользователей ВК*/
-                    if (args[1].equals("-all")) {
-                        HibernateUtil.buildEntityManagerFactory("OpenfmsPersistenceUnit");
-                        int a = 1;
-                        int[] ids = new int[500];
-
-                        while (true) {
-                            for (int i = 0; i < 500; i++) {
-                                ids[i] = a + i;
-                                System.out.println(a + i);
-                            }
-
-                            ArrayList<VkUser> vkUsers = VkParser.parse(ids);
-                            for (VkUser vkUserg : vkUsers) {
-                                try {
-                                    EntityManager entityManager = HibernateUtil.getEntityManager();
-                                    EntityTransaction entityTransaction = entityManager.getTransaction();
-                                    entityTransaction.begin();
-
-                                    entityManager.merge(vkUserg);
-
-                                    entityTransaction.commit();
-                                    entityManager.close();
-                                } catch (Exception e) {
-
-                                }
-                            }
-                            a += 500;
-                        }
-                    }
-                } else if (args.length == 3) {
-                    /*Адрес цели*/
-                    if (args[1].equals("-domain")) {
-                        VkUser vkUser;
-                        /*id*/
-                        if (args[2].matches("\\d+")) { //FIXME!!! Делать запрос через vk.utils.resolveScreenName и понимать, что является целью
-                            vkUser = VkParser.parse(Integer.parseInt(args[2]));
-                            printUserInfo(vkUser);
-                        /*Группа*/
-                        } else if (args[2].matches("-\\d+")) {
-                            System.out.println(VkGroupParser.parseUsers(Integer.parseInt(args[2].replace("-", ""))));
-                        }
-                    }
-                } else if (args.length == 5 && args[3].equals("-p")) {
-                    VkParser.setFieldsToParse(args[4]);
-                    VkUser vkUser;
-                    if (args[2].matches("\\d+")) {
-                        vkUser = VkParser.parse(Integer.parseInt(args[2]));
-                    } else {
-                        vkUser = VkParser.parse(args[2]);
-                    }
-
-                    Class clazz = vkUser.getClass();
-
-                    System.out.println(clazz.getDeclaredMethod("get" + args[4]).invoke(vkUser));
-
+            } else if (commandLine.hasOption("gu")) {
+                String targetDomain = commandLine.getOptionValue("g");
+                int targetId;
+                if (targetDomain.matches("\\d+")) {
+                    targetId = Integer.parseInt(targetDomain);
                 } else {
-                    System.out.println("Не верная конфигурация");
+                    targetId = VkParser.getUserId(targetDomain);
                 }
-
-            /*Если аргументов нет*/
-            } else {
-                System.out.println("Не верная конфигурация");
+                System.out.println(VkGroupParser.parseUsers(targetId)); //FIXME! построчный вывод каждого id в parseUsers (не всем скопом)
             }
-        } else {
-            System.out.println("Не верная конфигурация");
         }
 
     }
@@ -253,11 +204,13 @@ public class Main {
     private static void printUserInfo(VkUser vkUser) {
         System.out.println("Имя: " + vkUser.getFirstName());
         System.out.println("Фамилия: " + vkUser.getLastName());
-        System.out.println("Отчество (ник): " + vkUser.getNickname());
+        if (vkUser.getNickname() != null)
+            System.out.println("Отчество (ник): " + vkUser.getNickname());
 
-        if (!vkUser.getSex()) {
-            System.out.println("Девичья фамилия: " + vkUser.getMaidenName());
-        }
+        if (vkUser.getMaidenName() != null)
+            if (!vkUser.getSex()) {
+                System.out.println("Девичья фамилия: " + vkUser.getMaidenName());
+            }
 
         System.out.print("Пол: ");
         if (vkUser.getSex()) {
@@ -266,26 +219,33 @@ public class Main {
             System.out.println("Женский");
         }
 
-        System.out.println("Дата рождения: " + vkUser.getBirthday());
-        System.out.println("Скриннейм: " + vkUser.getScreenName());
-        System.out.println("Сайт: " + vkUser.getSite());
-        System.out.println("Твиттер: " + vkUser.getTwitter());
-        System.out.println("Скайп: " + vkUser.getSkype());
-        System.out.println("Инстаграм: " + vkUser.getInstagram());
-        System.out.println("Статус: " + vkUser.getStatus());
+        if (vkUser.getBirthday() != null)
+            System.out.println("Дата рождения: " + vkUser.getBirthday());
 
-        System.out.print("Страна: ");
+        if (vkUser.getScreenName() != null)
+            System.out.println("Скриннейм: " + vkUser.getScreenName());
+
+        if (vkUser.getSite() != null)
+            System.out.println("Сайт: " + vkUser.getSite());
+
+        if (vkUser.getTwitter() != null)
+            System.out.println("Твиттер: " + vkUser.getTwitter());
+
+        if (vkUser.getSkype() != null)
+            System.out.println("Скайп: " + vkUser.getSkype());
+
+        if (vkUser.getInstagram() != null)
+            System.out.println("Инстаграм: " + vkUser.getInstagram());
+
+        if (vkUser.getStatus() != null && !vkUser.getStatus().equals(""))
+            System.out.println("Статус: " + vkUser.getStatus());
+
         if (vkUser.getCountry() != null) {
-            System.out.println(vkUser.getCountry().getTitle());
-        } else {
-            System.out.println();
+            System.out.println("Страна: " + vkUser.getCountry().getTitle());
         }
 
-        System.out.print("Город: ");
         if (vkUser.getCity() != null) {
-            System.out.println(vkUser.getCity().getTitle());
-        } else {
-            System.out.println();
+            System.out.println("Город: " + vkUser.getCity().getTitle());
         }
     }
 }
